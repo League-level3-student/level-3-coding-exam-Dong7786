afb4113b8402c4fb4b90e6ee5ba8f788b773948c
League Student <league@iMac.attlocal.net>
2019-06-15 15:58:52 -0700

    adsf

--- test/test.java
+++ test/test.java
@@ -4,11 +4,14 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 public class test {
+	String h;
 	public static void main(String[] args) {
 		test t = new test();
 	}
 
 	test() {
+		String[][][][][][][][][][][][][][][][][][][][] s = new String[0][][][][][][][][][][][][][][][][][][][];
+		
 		HashMap<String, String> string = new HashMap<String, String>();
 		string.put("One", "Thing");
 		string.put("Two", "Thing");
@@ -16,8 +19,8 @@ public class test {
 		string2.put("One", "Thing");
 		string2.put("Two", "Thing");
 
-Object[] s = string.keySet().toArray();
-System.out.println(s[0]);
+Object[] s1 = string.keySet().toArray();
+System.out.println(s1[0]);
 
 
 System.out.println(string.entrySet());
