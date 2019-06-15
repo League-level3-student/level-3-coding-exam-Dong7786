a2f3747861c363652ca07204122fda1ac47c4bbe
League Student <league@iMac.attlocal.net>
2019-06-15 15:04:59 -0700

    Finished Now

--- test/HashMapCalculator.java
+++ test/HashMapCalculator.java
@@ -2,17 +2,31 @@ import java.util.HashMap;
 
 public class HashMapCalculator {
 
-	public Integer commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
+	public Object commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
 		// TODO Auto-generated method stub
+		Object[] o1 = hashmap1.keySet().toArray();
+		Object[] o2 = hashmap2.keySet().toArray();
 		
-		for(int i = 0; i < hashmap1.size(); i ++) {
+		int equal = 0;
+		for(int i = 0; i < hashmap1.size(); i++) {
 			
-			for(int o = 0; o < hashmap2.size(); o ++) {
-				if(hashmap1.get(i)
-				String[] s = hashmap1.keySet().toArray();
+			for(int h = 0; h < hashmap2.size(); h ++) {
+				System.out.println(i+", "+h+ ", "+(hashmap2.size() -1));
+				if(o1[i] == o2[h]) {
+					
+					if(hashmap1.get(o1[i]).equals(hashmap2.get(o2[h]))) {
+						equal ++;
+					}
+					
+					
+				}
+				
 			}
 		}
-		return null;
+		
+		return equal;
+		
 	}
 
+	
 }
